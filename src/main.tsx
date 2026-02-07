0b2ca84f5d3494952a52ac91c751e11f2b828b65
SA1NT <galaktik213@gmail.com>
2026-02-08 01:43:34 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import '../style.css'
 import './overrides.css'
+import './uno.css'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
